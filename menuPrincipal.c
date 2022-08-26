96a7697f15e01924cd1c2f83c95388aba9e93103
Day <107324462+DayXL@users.noreply.github.com>
2022-08-26 10:41:42 -0300

    Colocando estrurura de decisao

--- menuPrincipal.c
+++ menuPrincipal.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 
-void verMenuPrincipal(void);
+char verMenuPrincipal();
 void verMenuSobre(void);
 
 int main(void) {
-    verMenuPrincipal();
-    verMenuSobre();
+    char esc = verMenuPrincipal();
+
+    switch (esc!=0)
+        {
+            case 1:
+                printf("Módulo em desenvolvimento!");
+                break;
+
+            case 2:
+                printf("Módulo em desenvolvimento!");
+                break;
+            
+            case 3:
+                printf("Módulo em desenvolvimento!");
+                break;
+            
+            case 4:
+                printf("Módulo em desenvolvimento!");
+                break;
+
+            case 5:
+                printf("Módulo em desenvolvimento!");
+                break;
+
+            default:
+                printf("Programa finalizado!");
+                break;
+        }
+
     return 0;
 }
 
-void verMenuPrincipal(void) {
+char verMenuPrincipal() {
     printf("\n");
     printf("===============================================================================\n");
     printf("===                                                                         ===\n");
@@ -19,11 +46,18 @@ void verMenuPrincipal(void) {
     printf("===            2. Módulo em desenvolvimento                                 ===\n");
     printf("===            3. Módulo em desenvolvimento                                 ===\n");
     printf("===            4. Módulo em desenvolvimento                                 ===\n");
-    printf("===            5. Módulo em desenvolvimento                                 ===\n");
+    printf("===            5. Menu Sobre                                                ===\n");
     printf("===            0. Sair                                                      ===\n");
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
+
+    printf("Selecione sua opção:");
+
+    int esc;
+    scanf("&d" , esc);
+    return esc;
+
 }
 
 
