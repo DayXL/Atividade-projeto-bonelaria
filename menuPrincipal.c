d805356ebb1db043142dce2f68ba72a2e1f14c36
Day <107324462+DayXL@users.noreply.github.com>
2022-08-27 12:49:54 -0300

    Possiveis modelos para menu

--- menuPrincipal.c
+++ menuPrincipal.c
@@ -1,67 +1,107 @@
 #include <stdio.h>
 
-char verMenuPrincipal();
-void verMenuSobre(void);
+int verMenuPrincipal();
+void verMenuSobre();
 
 int main(void) {
-    char esc = verMenuPrincipal();
+    verMenuPrincipal();
+    verMenuSobre();
 
-    switch (esc!=0)
-        {
-            case 1:
-                printf("Módulo em desenvolvimento!");
-                break;
+    //int esc = verMenuPrincipal();//
 
-            case 2:
-                printf("Módulo em desenvolvimento!");
-                break;
-            
-            case 3:
-                printf("Módulo em desenvolvimento!");
-                break;
-            
-            case 4:
-                printf("Módulo em desenvolvimento!");
-                break;
+    /*while (esc!=0){
 
-            case 5:
-                printf("Módulo em desenvolvimento!");
-                break;
+        if (esc==1){
+            printf("Módulo em desenvolvimento!1");
 
-            default:
-                printf("Programa finalizado!");
-                break;
         }
+
+        else if (esc==2){
+            printf("Módulo em desenvolvimento!2");
+
+        }
+
+        else if (esc==3){
+            printf("Módulo em desenvolvimento!3");
+
+        }
+
+        else if (esc==4){
+            printf("Módulo em desenvolvimento!4");
+
+        }
+
+        else if (esc==5){
+            verMenuSobre();
+
+        }
+
+        else{
+            printf("Opção inválida!");
+        }
+        
+
+        /*switch (esc)
+            {
+                case 1:
+                    printf("Módulo em desenvolvimento!1");
+                    break;
+
+                case 2:
+                    printf("Módulo em desenvolvimento!2");
+                    break;
+                
+                case 3:
+                    printf("Módulo em desenvolvimento!3");
+                    break;
+                
+                case 4:
+                    printf("Módulo em desenvolvimento!4");
+                    break;
+
+                case 5:
+                    verMenuSobre();
+                    break;
+
+                default:
+                    printf("Opção inválida!");
+                    break;
+            } */
+
+        //int esc = verMenuPrincipal();
+    //}//
+
+    //printf("Programa finalizado!");//
 
     return 0;
 }
 
-char verMenuPrincipal() {
+int verMenuPrincipal() {
     printf("\n");
     printf("===============================================================================\n");
     printf("===                                                                         ===\n");
-    printf("===         = = = = = Sistema de Gestão de Bonelaria = = = = =              ===\n");
+    printf("===                   = = = = = SIG HAT = = = = =                           ===\n");
     printf("===                                                                         ===\n");
-    printf("===            1. Módulo em desenvolvimento                                 ===\n");
-    printf("===            2. Módulo em desenvolvimento                                 ===\n");
-    printf("===            3. Módulo em desenvolvimento                                 ===\n");
-    printf("===            4. Módulo em desenvolvimento                                 ===\n");
-    printf("===            5. Menu Sobre                                                ===\n");
-    printf("===            0. Sair                                                      ===\n");
+    printf("===                 1. Módulo em desenvolvimento                            ===\n");
+    printf("===                 2. Módulo em desenvolvimento                            ===\n");
+    printf("===                 3. Módulo em desenvolvimento                            ===\n");
+    printf("===                 4. Módulo em desenvolvimento                            ===\n");
+    printf("===                 5. Menu Sobre                                           ===\n");
+    printf("===                 0. Sair                                                 ===\n");
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
 
     printf("Selecione sua opção:");
-
     int esc;
-    scanf("&d" , esc);
+    scanf("&d" , &esc);
+
     return esc;
 
 }
 
 
-void verMenuSobre(void) {
+void verMenuSobre() {
     printf("\n");
     printf("===============================================================================\n");
     printf("===                                                                         ===\n");
