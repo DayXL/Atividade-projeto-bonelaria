ac4c0b173771eb0ddd2b3adbdc37bc481c2aed1f
Day <107324462+DayXL@users.noreply.github.com>
2022-09-03 18:02:23 -0300

    Dividindo em arquivos

--- menuPrincipal.c
+++ menuPrincipal.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "funcaoAux.c"
 
 char verMenuPrincipal(void);
 
-void moduloBone(void);
-char verMenuBone(void);
 
 void moduloFornecedor(void);
 char verMenuFornecedor(void);
@@ -87,69 +84,6 @@ char verMenuPrincipal(void) {
     printf("===                 5. Relatórios                                           ===\n");
     printf("===                 6. Menu Sobre                                           ===\n");
     printf("===                 0. Sair                                                 ===\n");
-    printf("===                                                                         ===\n");
-    printf("===============================================================================\n");
-    printf("\n");
-
-    printf("Selecione sua opção:");
-    char esc;
-    scanf("%c" , &esc);
-    getchar();
-
-    return esc;
-
-}
-
-void moduloBone(void) {
-
-    char esc = verMenuBone();
-
-    while (esc!='0'){
-
-        if (esc=='1'){
-            printf("Cadastrar modelo\n");
-
-        }
-
-        else if (esc=='2'){
-            printf("Editar modelo\n");
-
-        }
-
-        else if (esc=='3'){
-            printf("Excluir modelo\n");
-
-        }
-
-        else if (esc=='4'){
-            printf("Pesquisar modelo\n");
-
-        }
-
-        else{
-            printf("Opção inválida!\n");
-        
-        }
-
-        passarEnter();
-
-        esc = verMenuBone();
-    }
-
-}
-
-char verMenuBone(void) {
-    system ( " clear||cls " );
-    printf("\n");
-    printf("===============================================================================\n");
-    printf("===                                                                         ===\n");
-    printf("===                 = = = = = Menu Boné = = = = =                           ===\n");
-    printf("===                                                                         ===\n");
-    printf("===                 1. Cadastrar modelo                                     ===\n");
-    printf("===                 2. Editar modelo                                        ===\n");
-    printf("===                 3. Excluir modelo                                       ===\n");
-    printf("===                 4. Pesquisar modelo                                     ===\n");
-    printf("===                 0. Voltar ao menu principal                             ===\n");
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
     printf("\n");
