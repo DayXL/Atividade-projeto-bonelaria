41fbbe578c2061ca4e8dfdaeb45b94287769a8a6
Day <107324462+DayXL@users.noreply.github.com>
2022-09-04 13:05:58 -0300

    Revert "Correcoes (códifos estavam baguncados)"
    This reverts commit e5b4ffadf0140485b433ce6eb3d87e552b348aa4.

--- moduloBone.c
+++ moduloBone.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "moduloBone.h"
 
 void moduloBone(void) {
 
--- moduloBone.h
+++ moduloBone.h
@@ -1,7 +1,6 @@
 #ifndef MODULOBONE_H
 #define MODULOBONE_H
 
-void moduloBone(void);
-char verMenuBone(void);
+void passarEnter(void);
 
 #endif
