b1f296ca48571f25aa153c517947adc09f07bcfd
Day <107324462+DayXL@users.noreply.github.com>
2022-09-04 12:45:37 -0300

    Revert "Separando moduloRelatorio"
    This reverts commit 40615d0a1b11c2c8241844362bfeb64bda9457f0.

--- menuPrincipal.c
+++ menuPrincipal.c
@@ -3,7 +3,6 @@
 #include "funcaoAux.c"
 #include "moduloBone.c"
 #include "moduloSobre.c"
-#include "moduloRelatorio.c"
 
 char verMenuPrincipal(void);
 
@@ -15,6 +14,9 @@ char verMenuCliente(void);
 
 void moduloEstoque(void);
 char verMenuEstoque(void);
+
+void moduloRelatorio(void);
+char verMenuRelatorio(void);
 
 int main(void) {
 
@@ -270,6 +272,63 @@ char verMenuEstoque(void) {
     printf("===                 1. Ver materiais em estoque                             ===\n");
     printf("===                 2. Pesquisar materias em estoque                        ===\n");
     printf("===                 3. Comprar materiais para estoque                       ===\n");
+    printf("===                 0. Voltar ao menu principal                             ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+
+    printf("Selecione sua opção:");
+    char esc;
+    scanf("%c" , &esc);
+    getchar();
+
+    return esc;
+
+}
+
+void moduloRelatorio(void) {
+
+    char esc = verMenuRelatorio();
+
+    while (esc!='0'){
+
+        if (esc=='1'){
+            printf("Relatório Boné\n");
+
+        }
+
+        else if (esc=='2'){
+            printf("Relatório Fornecedor\n");
+
+        }
+
+        else if (esc=='3'){
+            printf("Relatório cliente\n");
+
+        }
+
+        else{
+            printf("Opção inválida!\n");
+        
+        }
+
+        passarEnter();
+
+        esc = verMenuRelatorio();
+    }
+
+}
+
+char verMenuRelatorio(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===              = = = = = Menu Relatórios = = = = =                        ===\n");
+    printf("===                                                                         ===\n");
+    printf("===                 1. Relatório Boné                                       ===\n");
+    printf("===                 2. Relatório Fornecedor                                 ===\n");
+    printf("===                 3. Relatório cliente                                    ===\n");
     printf("===                 0. Voltar ao menu principal                             ===\n");
     printf("===                                                                         ===\n");
     printf("===============================================================================\n");
