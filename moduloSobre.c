0f2dc712a88f2722edf64dfa3c0fccbea4e149af
Day <107324462+DayXL@users.noreply.github.com>
2022-09-07 14:29:27 -0300

    Separado moduloSobre

---
+++ moduloSobre.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "moduloSobre.h"
+
+void verMenuSobre(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===              = = = = = Informações do projeto = = = = =                 ===\n");
+    printf("===                                                                         ===\n");
+    printf("===  Projeto desenvolvido na disciplina DCT1106 - Programação, para         ===\n");
+    printf("===  avaliação desse período.                                               ===\n");
+    printf("===  Aluna: Dayanne Xavier Lucena.                                          ===\n");
+    printf("===  Segundo período.                                                       ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+}
