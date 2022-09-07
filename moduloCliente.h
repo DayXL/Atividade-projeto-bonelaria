225b5ba979d22dd3ffab33e32a799ce2a18c381b
Day <107324462+DayXL@users.noreply.github.com>
2022-09-07 14:47:41 -0300

    Criado cabecalho de moduloCliente

---
+++ moduloCliente.h
@@ -0,0 +1,8 @@
+
+void moduloCliente(void);
+char verMenuCliente(void);
+void cadastrarCliente(void);
+void editarCliente(void);
+void excluirCliente(void);
+void pesquisarCliente(void);
+void pedidoCliente(void);
