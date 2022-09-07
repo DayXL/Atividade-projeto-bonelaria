695a53b429cf44bd8aabcf6bdf60cc04d792f4b0
Day <107324462+DayXL@users.noreply.github.com>
2022-09-07 14:47:23 -0300

    Criado cabecalho de moduloFornecedor

---
+++ moduloFornecedor.h
@@ -0,0 +1,7 @@
+
+void moduloFornecedor(void);
+char verMenuFornecedor(void);
+void cadastrarFornecedor(void);
+void editarFornecedor(void);
+void excluirFornecedor(void);
+void pesquisarFornecedor(void);
