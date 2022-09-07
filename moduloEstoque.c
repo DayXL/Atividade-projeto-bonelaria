9aa895346f7f7c34a23fde0c0c3978b5bb0af50f
Day <107324462+DayXL@users.noreply.github.com>
2022-09-07 14:52:06 -0300

    Separado moduloEstoque

---
+++ moduloEstoque.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "moduloEstoque.h"
+#include "funcoesAux.h"
+
+void moduloEstoque(void) {
+
+    char esc = verMenuEstoque();
+
+    while (esc!='0'){
+
+        if (esc=='1'){
+            printf("Materias em estoque\n");
+
+        }
+
+        else if (esc=='2'){
+            printf("Pesquisar materias em estoque\n");
+
+        }
+
+        else if (esc=='3'){
+            printf("Comprar materias para estoque\n");
+
+        }
+
+        else{
+            printf("Opção inválida!\n");
+        
+        }
+
+        passarEnter();
+
+        esc = verMenuEstoque();
+    }
+
+}
+
+char verMenuEstoque(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===               = = = = = Menu Estoque = = = = =                          ===\n");
+    printf("===                                                                         ===\n");
+    printf("===                 1. Ver materiais em estoque                             ===\n");
+    printf("===                 2. Pesquisar materias em estoque                        ===\n");
+    printf("===                 3. Comprar materiais para estoque                       ===\n");
+    printf("===                 0. Voltar ao menu principal                             ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+
+    char esc;
+    esc = auxEscolha();
+
+    return esc;
+
+}
