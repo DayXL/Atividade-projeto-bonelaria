69340ee2ce5b994ee4b1560d49d2e339cecdee15
Day <107324462+DayXL@users.noreply.github.com>
2022-09-07 14:20:27 -0300

    Separa funcoes do modulo bone

---
+++ moduloBone.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "moduloBone.h"
+#include "funcoesAux.h"
+
+void moduloBone(void) {
+
+    char esc = verMenuBone();
+
+    while (esc!='0'){
+
+        if (esc=='1'){
+            cadastrarModelo();
+
+        }
+
+        else if (esc=='2'){
+            editarModelo();
+
+        }
+
+        else if (esc=='3'){
+            excluirModelo();
+
+        }
+
+        else if (esc=='4'){
+            pesquisarModelo();
+
+        }
+
+        else{
+            printf("Opção inválida!\n");
+        
+        }
+
+        passarEnter();
+
+        esc = verMenuBone();
+    }
+
+}
+
+char verMenuBone(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===                 = = = = = Menu Boné = = = = =                           ===\n");
+    printf("===                                                                         ===\n");
+    printf("===                 1. Cadastrar modelo                                     ===\n");
+    printf("===                 2. Editar modelo                                        ===\n");
+    printf("===                 3. Excluir modelo                                       ===\n");
+    printf("===                 4. Pesquisar modelo                                     ===\n");
+    printf("===                 0. Voltar ao menu principal                             ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+
+    char esc;
+    esc = auxEscolha();
+
+    return esc;
+
+}
+
+void cadastrarModelo(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===              = = = = = Cadastrar modelo = = = = =                       ===\n");
+    printf("===                                                                         ===\n");
+    printf("===                 Nome do modelo:                                         ===\n");
+    printf("===                 Unidades por metro de tecido:                           ===\n");
+    printf("===                 Unidades por tubo de linha:                             ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+
+}
+
+void editarModelo(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===                = = = = = Editar modelo = = = = =                        ===\n");
+    printf("===                                                                         ===\n");
+    printf("===                   Informe o modelo:                                     ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+
+}
+
+void excluirModelo(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===                = = = = = Excluir modelo = = = = =                        ===\n");
+    printf("===                                                                         ===\n");
+    printf("===                   Informe o modelo:                                     ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+
+}
+
+void pesquisarModelo(void) {
+    system ( " clear||cls " );
+    printf("\n");
+    printf("===============================================================================\n");
+    printf("===                                                                         ===\n");
+    printf("===                = = = = = Pesquisar modelo = = = = =                     ===\n");
+    printf("===                                                                         ===\n");
+    printf("===                   Informe o modelo:                                     ===\n");
+    printf("===                                                                         ===\n");
+    printf("===============================================================================\n");
+    printf("\n");
+
+}
+
