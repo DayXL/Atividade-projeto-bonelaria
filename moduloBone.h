7e90b815deb09a320b50938e75ea2e47b027cc21
Day <107324462+DayXL@users.noreply.github.com>
2022-09-07 14:20:13 -0300

    Criando cabecalho de moduloBone

---
+++ moduloBone.h
@@ -0,0 +1,8 @@
+//Assinatura das funções do módulo de bonés
+
+void moduloBone(void);
+char verMenuBone(void);
+void cadastrarModelo(void);
+void editarModelo(void);
+void excluirModelo(void);
+void pesquisarModelo(void);
