03c595a56ca533648fb94d69ff57f4ec3cd92c64
Day <107324462+DayXL@users.noreply.github.com>
2022-09-14 20:13:33 -0300

    Reformulando funcao

--- funcoesAux.h
+++ funcoesAux.h
@@ -4,4 +4,4 @@ void passarEnter(void);
 char auxEscolha(void);
 int validarPalavra(char *palavra);
 int validarNumInteiro(char *numero);
-int verificarCpf(int *cpf);
+int verificarCpf(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k);
