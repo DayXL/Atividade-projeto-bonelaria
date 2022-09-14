9c210b68bc4b468498702c83cb7362aad3537db0
Day <107324462+DayXL@users.noreply.github.com>
2022-09-14 10:52:40 -0300

    Criada primeira funcao validadora

--- funcoesAux.c
+++ funcoesAux.c
@@ -19,3 +19,37 @@ char auxEscolha(void) {
     return esc;
 
 }
+
+int validarPalavra(char *palavra) {
+    
+    int i = 0;
+
+    while (palavra[i] != '\0') {
+        //printf("%c\t", palavra[i]);
+        //printf("%d\n", palavra[i]);
+        
+
+        if (((palavra[i] >= 'a') && (palavra[i] <= 'z')) || (palavra[i] == ' ')) {
+            i = i + 1;
+            //printf("a");
+
+        }
+
+        else if ((palavra[i] >= 'A') && (palavra[i] <= 'Z')) {
+            i = i + 1;
+            //printf("b");
+
+        }
+
+        else {
+            //printf("\nfinal should be => %c", palavra[i]);
+            return 0;
+
+        }
+
+    }
+
+    //printf("s");
+    return 1;
+
+}
