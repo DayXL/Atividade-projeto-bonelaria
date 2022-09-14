23312fd2ab07d854c671ab255b744661e3c6eb5e
Day <107324462+DayXL@users.noreply.github.com>
2022-09-14 11:19:37 -0300

    Assinado nova funcao

--- moduloBone.h
+++ moduloBone.h
@@ -1,9 +1,13 @@
 //Assinatura das funções do módulo de bonés
 
+//Assinaturas principais
 void moduloBone(void);
 char verMenuBone(void);
 void cadastrarModelo(void);
 void editarModelo(void);
 void excluirModelo(void);
 void pesquisarModelo(void);
+
+//Assinaturas para validacao
 void validarNomeModelo(void);
+void validarUniPorMetro(void);
