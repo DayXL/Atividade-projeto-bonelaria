5fac3932bf23d99b60f1d8cc8837c9179f6ae1b3
DayXL <day.luc.xav0606@gmail.com>
2022-10-28 16:28:22 -0300

    criado struct para cliente

--- moduloCliente.h
+++ moduloCliente.h
@@ -1,4 +1,16 @@
 
+typedef struct cliente Cliente;
+
+struct cliente {
+
+    char nomeDoCliente[100];
+    char cpf[12];
+    char numero[12];
+    char email[100];
+    int ativo;
+
+};
+
 //Assinaturas principais
 
 void moduloCliente(void);
