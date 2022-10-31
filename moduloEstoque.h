fc115f7694970945a17693170fcd4acda8da9115
DayXL <day.luc.xav0606@gmail.com>
2022-10-31 10:02:55 -0300

    criado struct

--- moduloEstoque.h
+++ moduloEstoque.h
@@ -1,3 +1,13 @@
+typedef struct estoque Estoque;
+
+struct estoque {
+
+    char nomeDoMaterial[100];
+    char cnpj[15];
+    char quant[10];
+    char ativo[2];
+
+};
 
 void moduloEstoque(void);
 char verMenuEstoque(void);
@@ -6,5 +16,7 @@ void pesquisarMateriaisEstoque(void);
 void comprarMateriais(void);
 void cadastrarMateriais(void);
 void excluirMaterial(void);
-void validarNomeMaterial(void);
-void cnpj(void);
+void validarNomeMaterial(Estoque *est);
+void cnpj(Estoque *est);
+void validarQuant(Estoque *est);
+void salArqEst(Estoque* est);
