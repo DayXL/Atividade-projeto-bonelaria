7a0358814b456112d0af70fd045af00ef9f09173
DayXL <day.luc.xav0606@gmail.com>
2022-10-31 09:23:16 -0300

    criado struct

--- moduloBone.h
+++ moduloBone.h
@@ -1,3 +1,15 @@
+typedef struct boneChap BoneChap;
+
+struct boneChap {
+
+    char nomeBonChap[100];
+    char codigo[6];
+    char uniPorMetro[3];
+    char uniPorTubo[3];
+    char ativo[2];
+
+};
+
 //Assinatura das funções do módulo de bonés
 
 //Assinaturas principais
@@ -7,15 +19,15 @@ char escolherModelo(void);
 void editarModelo(void);
 void excluirModelo(void);
 void pesquisarModelo(void);
-void cadastrarModelo(void);
+void cadastrarModelo(BoneChap *bcp);
 void escolhendoModelo(void);
 
 //Assinaturas para validacao
-void validarNomeModelo(void);
-void validarUniPorMetro(void);
-void validarUniPorTubo(void);
+void validarNomeModelo(BoneChap *bcp);
+void validarUniPorMetro(BoneChap *bcp);
+void validarUniPorTubo(BoneChap *bcp);
 void telaBone(void);
 void telaChapeu(void);
 char* validarEsc(char tipo);
-void gerarCodigoBone(char *esc, int tam, char num);
-void gerarCodigoChapeu (char *esc, int tam, char num);
+void gerarCodigoBone(char *esc, int tam, char num, BoneChap *bcp);
+void gerarCodigoChapeu (char *esc, int tam, char num, BoneChap *bcp);
