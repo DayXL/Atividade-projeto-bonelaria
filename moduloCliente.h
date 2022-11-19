aed993b7665dfa242bdf1ff856ec1287853d3dd4
DayXL <day.luc.xav0606@gmail.com>
2022-11-19 17:18:03 -0300

    separando modulos

--- moduloCliente.h
+++ moduloCliente.h
@@ -1,6 +1,5 @@
 
 typedef struct cliente Cliente;
-typedef struct pedidoCliente PedidoCliente;
 typedef struct boneChap BoneChap;
 typedef struct estoque Estoque;
 
@@ -14,15 +13,7 @@ struct cliente {
 
 };
 
-struct pedidoCliente {
 
-    char pedido[20];
-    char cpf[12];
-    char codigo[6];
-    float quant;
-    char cor[30];
-
-};
 
 //Assinaturas principais
 
@@ -32,7 +23,6 @@ void cadastrarCliente(void);
 void editarCliente(void);
 void excluirCliente(void);
 void pesquisarCliente(void);
-void pedidoCliente(void);
 
 void validarNomeCliente(char *nomeCliente);
 int validarCPF(char *cpf);
@@ -43,15 +33,3 @@ void lerArqClt(int num);
 void exibCliente(Cliente *clt);
 Cliente* acharClt(char *cpf);
 char telAtlClt(void);
-BoneChap* selecionarModelo(char *codigo);
-void lerArqBcpDif(char num);
-void lerQuant(char *quant);
-int verMtlDisp(BoneChap* bcp, float quant, float *novoValor, char *aux);
-void acharTec(void);
-int selecionarCor(char *aux, float quant, BoneChap* bcp, float *novoValor, char * cor);
-Estoque* acharMtlPelNom(char* nome, int tam);
-void alterValor(float *novoValor, char *aux, int quant, Estoque* est);
-void salArqPedClt(PedidoCliente* pedClt);
-void lerArqPedClt(void);
-void exibPedido(PedidoCliente *pedClt);
-char* gerarIdPed(void);
