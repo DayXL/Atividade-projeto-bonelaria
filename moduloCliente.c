e3fccd76a8b229e1a56b9977e77586ae3e2ac33a
DayXL <day.luc.xav0606@gmail.com>
2022-11-19 14:40:07 -0300

    criado leitura de pedidos

--- moduloCliente.c
+++ moduloCliente.c
@@ -1191,3 +1191,36 @@ void salArqPedClt(PedidoCliente* pedClt) {
     fclose(fp);
 
 }
+
+void lerArqPedClt(void) {
+    
+    FILE *fp;
+    PedidoCliente *pedClt;
+    
+    if (access("arqPedClt.dat", F_OK) != -1) {
+
+        fp = fopen("arqPedClt.dat","rb");
+
+        if (fp == NULL) {
+            printf("Erro com arquivo!");
+
+        }
+
+        else {
+
+            pedClt = (PedidoCliente*) malloc(sizeof(PedidoCliente));
+
+            while (fread(pedClt, sizeof(PedidoCliente), 1, fp)) {
+
+                exibPedClit(clt);
+
+            }
+
+            free(pedClt);
+
+        }
+
+        fclose(fp);
+    }
+
+}
